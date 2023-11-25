f388e1b210bce7bf86448f79d0e40c40282c8d53
Fadora <fabiandora26@gmail.com>
2023-11-25 23:49:59 +0100

    parent appointment ui ok, data not connected

--- frontend/src/parent/BookAppointment.jsx
+++ frontend/src/parent/BookAppointment.jsx
@@ -7,21 +7,28 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import 'dayjs/locale/en-gb'
 import { Button, FormControl, InputLabel, MenuItem, Paper, Select } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 
 
 
 
 export default function BookAppointment(){
+
+    const navigate = useNavigate();
    
-   const lastMonday = dayjs().startOf('week');
+    const lastMonday = dayjs().startOf('week');
     const nextSunday = dayjs().endOf('week').startOf('day');
 
-    const [times, setTime] = useState([1,2,3,4,5])
+    const [times, setTime] = useState([15,13,14,17])
     const [selectedTime, setSelectedTime] = useState('')
-    const [selectedDate, setSelectedDate] = useState(dayjs('2022-04-17'))
+    const [selectedDate, setSelectedDate] = useState(dayjs(new Date()))
+    const [selectedCase, setSelectedCase] = useState("case4")
     const [appointments, setAppointments] = useState([lastMonday,nextSunday ])
     
+    const navigateToAppointments = () => {
+        navigate("/myappointments")
+    }
     const isAvailable = (date) => {
         console.log(dayjs(date).format('DD/MM/YYYY'));
         console.log("Last Monday "+dayjs(lastMonday).format('DD/MM/YYYY'));
@@ -41,13 +48,15 @@ export default function BookAppointment(){
             <Paper className="date-picker-container">
                 <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
                 <StaticDatePicker
+                    disablePast
                     value={selectedDate}
                     onChange={(e) => setSelectedDate(e)}
                     shouldDisableDate={isAvailable}></StaticDatePicker>
              </LocalizationProvider>
             </Paper>
-            <Paper>
-                 <FormControl sx={{ width: 250 }}>
+            <Paper className="time-picker-container">
+                 
+                <FormControl sx={{ width: 250 }} className="time-picker-form-field">
                         <InputLabel id="demo-simple-select-label">Time</InputLabel>
                         <Select
                         labelId="demo-simple-select-label"
@@ -61,8 +70,30 @@ export default function BookAppointment(){
                                     )}
                         </Select>
                 </FormControl>
-                <Button>Book selected appointment</Button>
-                <h2>{selectedDate.format('DD/MM/YYYY')}</h2>
+
+                <FormControl sx={{ width: 250 }} className="time-picker-form-field">
+                        <InputLabel id="demo-simple-select-label">Case</InputLabel>
+                        <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={selectedCase}
+                        label="Case"
+                        onChange={(e) => setSelectedCase(e.target.value)}
+                        >
+                          
+                                    <MenuItem value={"case1"}>{"case1"}</MenuItem>
+                                    <MenuItem value={"case2"}>{"case2"}</MenuItem>
+                                    <MenuItem value={"case3"}>{"case3"}</MenuItem>
+                               
+                                    
+                        </Select>
+                </FormControl>
+
+                <div>Selected appointment:</div>
+                 <h3>{selectedDate.format('MM.DD')} {selectedTime}</h3>
+                <Button sx={{margin:2}} variant="contained" color="secondary" >Book it</Button>
+                <Button sx={{margin:1}} variant="outlined" color="secondary" onClick={navigateToAppointments}>Cancel</Button>
+               
             </Paper>
              
         </Paper>
